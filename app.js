9e9cdf5d1d4901a74b7448a147444e192b1a59c8
kostyan <kostyan@kostyans-MacBook-Air.local>
2023-08-17 13:51:41 +0300

    add mongodb and changed post request to every post

--- app.js
+++ app.js
@@ -2,15 +2,38 @@ import express from "express";
 import bodyParser from "body-parser";
 import ejs from "ejs";
 import _ from "lodash";
+import mongoose from "mongoose";
 
-const homeStartingContent =
-  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const homeStartingContent = {
+  title: "Starting post",
+  content:
+    "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.",
+};
 const aboutContent =
   "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-let posts = [];
+mongoose.connect("mongodb://localhost:27017/blogDB");
+
+const postSchema = mongoose.Schema({
+  title: String,
+  content: String,
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+const post1 = new Post({
+  title: "Day 1",
+  content: "Start writing my own post!",
+});
+const post2 = new Post({
+  title: "Day 2",
+  content: "Let's wright some interesting post",
+});
+const post3 = new Post({ title: "Day 3", content: "New cool post" });
+
+const defaultPosts = [post1, post2, post3];
 
 const app = express();
 
@@ -19,11 +42,14 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("home", {
-    homeStartingContent: homeStartingContent,
-    posts: posts,
-  });
+app.get("/", async (req, res) => {
+  const foundPosts = await Post.find({});
+  if (foundPosts.length === 0) {
+    await Post.insertMany(defaultPosts);
+    res.redirect("/");
+  } else {
+    res.render("home", { posts: foundPosts });
+  }
 });
 
 app.get("/about", (req, res) => {
@@ -39,26 +65,28 @@ app.get("/compose", (req, res) => {
 });
 
 app.post("/compose", (req, res) => {
-  const post = {
-    title: req.body.postTitle,
-    content: req.body.postBody,
-  };
+  const postTitle = req.body.postTitle;
+  const postContent = req.body.postBody;
+  const post = new Post({
+    title: postTitle,
+    content: postContent,
+  });
+  post.save();
 
-  posts.push(post);
   res.redirect("/");
 });
 
-app.get("/posts/:title", (req, res) => {
-  const requestedTitle = _.lowerCase(req.params.title);
-  posts.forEach((post) => {
-    const storedTitle = _.lowerCase(post.title);
-    if (storedTitle === requestedTitle) {
-      res.render("post", { post: post });
-    } else {
-      console.log("Not found!");
-    }
+app.get("/posts/:_id", async (req, res) => {
+  const requestedPostId = req.params._id;
+  const requestedPost = await Post.findOne({ _id: requestedPostId });
+  res.render("post", {
+    title: requestedPost.title,
+    content: requestedPost.content,
   });
+  console.log(requestedPost);
 });
+
+app.post("/delete", (req, res) => {});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
